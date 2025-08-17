1b183b8736adb8897a65eef47b52f86ec9ec00b9
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2025-08-17 19:55:05 +0600

    Create User_profile_display.php

---
+++ User_profile_display.php
@@ -0,0 +1,34 @@
+// Scenario: Your web application needs to display a user's profile information, which is stored in a
+// structured way.
+// • Topics Covered: Associative Arrays, foreach loop.
+// • Sample Input:
+// PHP
+// $userProfile = [
+//  "firstName" => "John",
+//  "lastName" => "Doe",
+//  "email" => "john.doe@example.com",
+//  "city" => "New York"
+// ];
+// • Expected Output:
+// • First Name: John
+// • Last Name: Doe
+// • Email: john.doe@example.com
+// • City: New York
+
+
+<?php
+$userProfile = [
+ "firstName" => "John",
+ "lastName" => "Doe",
+ "email" => "john.doe@example.com",
+ "city" => "New York"
+];
+echo "First Name: ".$userProfile["firstName"];
+echo "<br>";
+echo "Last Name: ".$userProfile["lastName"];
+echo "<br>";
+echo "Email: ".$userProfile["email"];
+echo "<br>";
+echo "City: ".$userProfile["city"];
+echo "<br>";
+?>
