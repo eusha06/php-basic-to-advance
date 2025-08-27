00b8644d6d9bc6a5b9754ef5c25f382789f66631
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2025-08-27 21:11:40 +0600

    Create 8.php

---
+++ 8.php
@@ -0,0 +1,6 @@
+<?php
+echo calculateArea(5, 10);
+function calculateArea($length, $width) {
+    return "The area of a rectangle with length ".$length." and width ".$width." is ".($length*$width).".";
+}
+?>
